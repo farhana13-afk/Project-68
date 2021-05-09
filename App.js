06f4143cf7caf000f32e2018f8ff798eccd34630
farhana13-afk <73855710+farhana13-afk@users.noreply.github.com>
2021-05-09 19:07:46 -0400

    Update App.js

--- App.js
+++ App.js
@@ -4,23 +4,11 @@ import Instagram from './screens/in';
 import {View} from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
-import {Header} from 'react-native-elements';
 
-export default class App extends React.Component{
-  render(){
+export default function App() {
   return (
-    <View>
-    <Header
-          backgroundColor={'#00ff80'}
-          centerComponent={{
-            text: 'Buzz App',
-            style: { color: 'black', fontSize: 20 },
-          }}
-        />
     <AppContainer/>
-    </View>
   );
-}
 }
 
 const TabNavigator = createBottomTabNavigator({
