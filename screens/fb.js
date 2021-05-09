135f44b45fa08948a159d9ecf50ca9961f42368f
farhana13-afk <73855710+farhana13-afk@users.noreply.github.com>
2021-05-09 18:55:02 -0400

    Add files via upload

---
+++ screens/fb.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default class Facebook extends React.Component{
+
+render(){
+  return (
+    <View style={{flex:1, justifyContent: 'center', alignItems:'center'}}>
+        <Text style={{color:'blue'}}>Facebook</Text>
+    </View>
+  );
+}
+
+}
